f5d40791f04e5acb53238a85b86d3b039b562141
Eyerusalem Belay <eyuuutyake@gmail.com>
2026-03-29 23:26:13 +0300

    adding merry

---
+++ Controllers/merryController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelController : Controller
+{
+    // 
+    // GET: /HelloWorld/
+public IActionResult Index()
+{
+    return View();
+}
+    // GET: /HelloWorld/Welcome/ 
+    public string Welcome()
+    {
+        return "i will add another view for this one";
+    }
+}
--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7285434c77a858dbc5f8b2771599a87d5fcf06a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+556cfa21e6160e6cc08fa44634275cf75933ab9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
