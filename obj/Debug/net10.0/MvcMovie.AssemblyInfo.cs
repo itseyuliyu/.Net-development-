7ada0fb8aa5a74ee95be21667ff1fa19e3c2d59e
Eyerusalem Belay <eyuuutyake@gmail.com>
2026-03-30 22:12:54 +0300

    changing the layout

--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16b0fbd44e2158d454b7e9b855be4ded33f05bed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2b8985c94a8368d2c3ee67df676c6ac49ca0f7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
