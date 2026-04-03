3f28162e7a9d0274c50a1154ad7f2b7334de43c1
Eyerusalem Belay <eyuuutyake@gmail.com>
2026-04-03 21:49:42 +0300

    connecting with db

--- Program.cs
+++ Program.cs
@@ -1,27 +1,36 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MvcMovie.Data;
-
+using MvcMovie.Models;
 var builder = WebApplication.CreateBuilder(args);
 
-// ✅ Services MUST be added here (only once)
 builder.Services.AddDbContext<MvcMovieContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("MvcMovieContext") 
-    ?? throw new InvalidOperationException("Connection string 'MvcMovieContext' not found.")));
+    options.UseSqlite(builder.Configuration.GetConnectionString("MvcMovieContext")));
 
+// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure pipeline
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
+
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
 app.UseRouting();
+
 app.UseAuthorization();
 
 app.MapStaticAssets();
