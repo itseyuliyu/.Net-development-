c0bc201d13750b180656b23f9b35ac408a119437
Eyerusalem Belay <eyuuutyake@gmail.com>
2026-04-04 19:34:56 +0300

    adding modules for the database

--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f28162e7a9d0274c50a1154ad7f2b7334de43c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6500cfc35c170cb229ad312891cd24f34b22812")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
