04b5c1892663f991197117928deeb4c848c92493
Eyerusalem Belay <eyuuutyake@gmail.com>
2026-04-06 22:32:37 +0300

    adding model for login

--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e03d68578634b2fc2293ce62e5262b80bce2323")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+602ee0c1052d02205a19da9770e1fdcd17e67028")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
