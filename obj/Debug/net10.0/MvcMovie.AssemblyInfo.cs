7bfd7cd2f1f63c87ceb957626ec70e05f6a2342d
Eyerusalem Belay <eyuuutyake@gmail.com>
2026-04-15 22:35:12 +0300

    layout

--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24a35a2727f28f95eb9f57031ba6ef0b8d12ff30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+488c8e71a2116e935a158f5fd29a39170b083fb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
