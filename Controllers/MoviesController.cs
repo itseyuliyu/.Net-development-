5cebc81aeedf7163cba64cc0bcd84c1baf2e19d2
Eyerusalem Belay <eyuuutyake@gmail.com>
2026-04-18 22:50:13 +0300

    layout

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -3,7 +3,7 @@ using System;
 //using System.Linq;
 //using System.Threading.Tasks;
 //using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
+//using Microsoft.AspNetCore.Mvc.Rendering;
 //using Microsoft.EntityFrameworkCore;
 //using MvcMovie.Data;
 //using MvcMovie.Models;
